2f84b0a3e24e0848c0b772bcc421c52ee970679a
Scoutbirds <70183932+Scoutbirds@users.noreply.github.com>
2021-11-24 01:07:51 -0500

    Contact page almost done

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,9 +1,53 @@
 import React from 'react'
+import './Contact.css'
+import { GrLocation } from 'react-icons/gr';
+import { IoCallOutline } from 'react-icons/io5';
 
 export default function Contact() {
+
     return (
-        <div>
-            Contact
+        <div className = "contactPage">
+            <div className = "contactBody">
+                <div className= "contactInside">
+                    <div className = "visit">Visit our Location</div>
+                    
+                    <div className = "map">
+                        <iframe title="location" width="1200" height="550" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" 
+                        src="https://maps.google.com/maps?width=862&amp;height=585&amp;hl=en&amp;q=FIU%20Miami+(Locations)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+                    </div>
+                    <div className = "addressContainer">
+                        <div className="icon"><GrLocation size={60} /></div>
+                        <div className = "address">
+                            <div className = "addressTitle">ROCLA Miami</div>
+                            <div className = "addressInfo">
+                                11200 SW 8th St, 
+                                <br/>
+                                Miami, FL 33199
+                            </div>
+                        </div>
+                        <div className="icon"><IoCallOutline size={60} /></div>
+                        <div className = "address">
+                            <div className = "addressTitle">Phone Number</div>
+                            <div className = "addressInfo">
+                                +1 (123) 456-7891
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className = "messageBlock">
+                <form className = "messageContainer">
+                    <div className = "messageContact">Contact Us</div>
+                    <div className = "messageTitle">Name</div>
+                    <input type = "text" id = "name" placeholder="Your name.."/>
+                    <div className = "messageTitle">E-mail</div>
+                    <input type = "email" id = "e-mail" placeholder="example@gmail.com"/>
+                    <div className = "messageTitle">Message</div>
+                    <textarea type = "text" id = "message" placeholder="Message"/>
+                </form>
+            </div>
         </div>
+
     )
 }
--- src/Components/Procedures.js
+++ src/Components/Procedures.js
@@ -4,25 +4,24 @@ import './Procedures.css'
 
 export default function Procedures() {
 
-
-
-
     return (
         <div className = "procedurestab">
             <div className = "procedurestabType">Injectibles</div>
+
+            <div></div>
             <div className = "procedurestabcontainer">
                 <div className = "procedurestabcontainer2">
                     <div className = "procedurestabtitle">Botox, Dysport and Xeomin</div>
-                    <div className = "procedurestabinfo">
+                    <p className = "procedurestabinfo">
                     These are both a type of botulinum toxin that is used to prevent, 
                     treat and maintain a smoother and healthier looking skin, by relaxing 
                     those muscles that cause our worry lines, frown lines, crows feet and smokers’ line.
-                    </div>
+                    </p>
                 </div>
                 <div className = "procedurestabimg"></div>
             </div>
             <div className = "procedurestabcontainer">
-                <div className = "procedurestabcontainer2">
+                <div className = "procedurestabcontainer2 full">
                     <div className = "procedurestabtitle">Sculptra Aesthetic</div>
                     <div className = "procedurestabinfo">
                     Sculptra Aesthetic is an FDA-approved injectable that helps gradually 
@@ -30,24 +29,27 @@ export default function Procedures() {
                     </div>
                 </div>
             </div>
-            <div className = "procedurestabcontainer">
-                <div className = "procedurestabcontainer2">
-                    <div className = "procedurestabtitle">Juvederm</div>
-                    <div className = "procedurestabinfo">
-                        Modified (Hyaluronic acid) is the main ingredient behind JUVÉDERM ® . 
-                        Getting treated with JUVÉDERM ® helps add volume to different areas of the 
-                        face without surgery while also temporarily restoring the volume loss beneath the surface which we loose with aging.
+            <div className = "procedurestabfloat">
+                <div className = "procedurestabcontainer">
+                    <div className = "procedurestabcontainer2 full">
+                        <div className = "procedurestabtitle">Juvederm</div>
+                        <div className = "procedurestabinfo">
+                            Modified (Hyaluronic acid) is the main ingredient behind JUVÉDERM ® . 
+                            Getting treated with JUVÉDERM ® helps add volume to different areas of the 
+                            face without surgery while also temporarily restoring the volume loss beneath the surface which we loose with aging.
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className = "procedurestabcontainer">
-                <div className = "procedurestabcontainer2">
-                    <div className = "procedurestabtitle">Restylane</div>
-                    <div className = "procedurestabinfo">
-                    Restylane is an HA filler (hyaluronic acid) used to restore volume loss due to aging. 
-                    There are many types of Restylane filler which vary in consistency, and flexibility. Your medical aesthetic practitioner will recommend which is best suitable for you upon a thorough consultation.
+                <div className = "procedurestabcontainer">
+                    <div className = "procedurestabcontainer2 full">
+                        <div className = "procedurestabtitle">Restylane</div>
+                        <div className = "procedurestabinfo">
+                        Restylane is an HA filler (hyaluronic acid) used to restore volume loss due to aging. 
+                        There are many types of Restylane filler which vary in consistency, and flexibility. Your medical aesthetic practitioner will recommend which is best suitable for you upon a thorough consultation.
+                        </div>
                     </div>
                 </div>
+
             </div>
         </div>
     )
